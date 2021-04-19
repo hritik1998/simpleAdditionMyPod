a19fa26c9a53ff4222c8c4008400cba498651d5d
cedcoss <cedcoss@cedcosss-Mac-mini-3.local>
2021-04-19 17:51:29 +0530

    addded functins

---
+++ simpleAdditionMyPod/simpleAdditionMyPod.swift
@@ -0,0 +1,21 @@
+//
+//  simpleAdditionMyPod.swift
+//  simpleAdditionMyPod
+//
+//  Created by cedcoss on 19/04/21.
+//
+
+import Foundation
+public final class simpleAdditionMyPod {
+
+    let name = "simpleAdditionMyPod"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
