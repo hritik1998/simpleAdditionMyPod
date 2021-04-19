e31ca886fb4213b8fab534ba1135f64d0d93b4d0
cedcoss <cedcoss@cedcosss-Mac-mini-3.local>
2021-04-19 17:30:28 +0530

    Initial project setup

---
+++ simpleAdditionMyPod/simpleAdditionMyPod.h
@@ -0,0 +1,18 @@
+//
+//  simpleAdditionMyPod.h
+//  simpleAdditionMyPod
+//
+//  Created by cedcoss on 19/04/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for simpleAdditionMyPod.
+FOUNDATION_EXPORT double simpleAdditionMyPodVersionNumber;
+
+//! Project version string for simpleAdditionMyPod.
+FOUNDATION_EXPORT const unsigned char simpleAdditionMyPodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <simpleAdditionMyPod/PublicHeader.h>
+
+
